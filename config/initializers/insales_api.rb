bd1e7fd656aade52d785dca8e1c10c1680a4c1a0
Alexey Chuvashov <alexeychu@gmail.com>
2014-12-15 13:04:39 +0300

    api params over env variable

--- config/initializers/insales_api.rb
+++ config/initializers/insales_api.rb
@@ -1,4 +1,3 @@
-# InsalesApi::App.api_key            = ENV['INSALES_API_KEY']
-# InsalesApi::App.api_secret         = ENV['INSALES_API_SECRET']
-# InsalesApi::App.api_host           = ENV['INSALES_API_HOST']
-
+InsalesApi::App.api_key            = ENV['INSALES_API_KEY']
+InsalesApi::App.api_secret         = ENV['INSALES_API_SECRET']
+InsalesApi::App.api_host           = ENV['INSALES_API_HOST']
