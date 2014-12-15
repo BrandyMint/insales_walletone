64532755995ebe33ed398650dd57959ca3697267
Alexey Chuvashov <alexeychu@gmail.com>
2014-12-15 15:59:14 +0300

    remove to_json

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -53,7 +53,6 @@ class PaysController < ApplicationController
   def wmi_result
     p '*'*100
     p params
-    p params.to_json
     p '='*100
     @account = Account.find_by(walletone_shop_id: params[:WMI_MERCHANT_ID])
     responce_signature = params.delete :WMI_SIGNATURE
