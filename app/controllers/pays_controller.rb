201f49f7f1c82bbc27ecc0507c6e1eae5d0905bb
Alexey Chuvashov <alexeychu@gmail.com>
2014-12-16 11:44:38 +0300

    recoctoring pays and other

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -5,78 +5,90 @@ class PaysController < ApplicationController
   #insales doc https://wiki.insales.ru/wiki/%D0%9F%D0%BE%D0%B4%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%BD%D0%B5%D1%88%D0%BD%D0%B5%D0%B3%D0%BE_%D1%81%D0%BF%D0%BE%D1%81%D0%BE%D0%B1%D0%B0_%D0%BE%D0%BF%D0%BB%D0%B0%D1%82%D1%8B_(%D0%B4%D0%BB%D1%8F_%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%D0%BE%D0%B2_%D0%B8%D0%BD%D1%82%D0%B5%D0%B3%D1%80%D0%B0%D1%86%D0%B8%D0%B9)
   #walletone doc http://www.walletone.com/ru/merchant/documentation/
   def pay
-    walletone_shop_id = params[:shop_id]
-    amount = params[:amount]
-    transaction_id = params[:transaction_id]
-    description = params[:description]
-    key = params[:key]
-    order_id = params[:order_id]
-    phone = params[:phone]
-    email = params[:email]
-
-    @account = Account.find_by(walletone_shop_id: walletone_shop_id)
-    insales_app = InsalesApi::App.new(@account.domain, @account.password)
-    insales_app.configure_api
-
-    order = InsalesApi::Order.find(order_id)
-    fio = order.client.name
-    client_surname, client_name = fio.split(' ')
-
-    wmi_params = {
-        WMI_MERCHANT_ID: walletone_shop_id,
-        WMI_PAYMENT_AMOUNT: amount,
-        WMI_CURRENCY_ID: @account.walletone_currency,
-        WMI_PAYMENT_NO: transaction_id,
-        WMI_DESCRIPTION: "BASE64:#{Base64.encode64(description)}",
-        WMI_SUCCESS_URL: insales_order_path(key),
-        WMI_FAIL_URL: insales_order_path(key),
-        WMI_RECIPIENT_LOGIN: email || phone,
-        WMI_CUSTOMER_FIRSTNAME: client_name,
-        WMI_CUSTOMER_LASTNAME: client_surname,
-        WMI_CUSTOMER_EMAIL: email,
-        amount: amount,
-        transaction_id: transaction_id,
-        key: key,
-        order_id: order_id
-    }
-
-    signature = walletone_signature(wmi_params, @account.walletone_password)
-    wmi_params[:WMI_SIGNATURE] = signature
-
+    @account = Account.find_by(walletone_shop_id: params[:shop_id])
+    initialize_api(@account)
+    wmi_params = calculate_walletone_params(params)
     walletone_uri = URI(configus.walletone_payment_url)
     walletone_uri.query = wmi_params.to_query
     redirect_to walletone_uri.to_s
   end
 
-  def wmi_result
+  def walletone_result
     @account = Account.find_by(walletone_shop_id: params[:WMI_MERCHANT_ID])
-    responce_signature = params.delete :WMI_SIGNATURE
-    calculated_signature = walletone_signature(params.except(:action, :controller), @account.walletone_password)
-    if responce_signature == calculated_signature && params[:WMI_ORDER_STATE] == 'Accepted'
-      insales_params = { #порядок важен для вычисления подписи
-          shop_id: params[:WMI_MERCHANT_ID],
-          amount: params[:WMI_PAYMENT_AMOUNT],
-          transaction_id: params[:transaction_id],
-          key: params[:key],
-          paid: 1
-      }
-      insales_params[:signature] = insales_signature(insales_params, @account.walletone_password)
-      HTTParty.post(insales_result_path(:success), body: insales_params)
+    initialize_api(@account)
+    @payment_gateway = InsalesApi::PaymentGateway.find(@account.payment_gateway_id)
+    render text: 'WMI_RESULT=RETRY&WMI_DESCRIPTION=invalid signature' unless payment_valid?(params)
+    render text: 'WMI_RESULT=RETRY&WMI_DESCRIPTION=invalid state' unless params[:WMI_ORDER_STATE] == 'Accepted'
+    insales_params = calculate_insales_params(params)
+    response = HTTParty.post(insales_result_path(:success), body: insales_params)
+    if response.code == '200'
       render text: 'WMI_RESULT=OK'
     else
-      HTTParty.post(insales_result_path(:fail))
-      render text: 'WMI_RESULT=RETRY&WMI_DESCRIPTION=fail signature'
+      render text: 'WMI_RESULT=RETRY&WMI_DESCRIPTION=server busy'
     end
   end
 
   private
+
+  def calculate_walletone_params(params)
+    client_surname, client_name = get_client_fio(params[:order_id])
+    order_path = insales_order_path(params[:key])
+    wmi_params = {
+        WMI_MERCHANT_ID: params[:shop_id],
+        WMI_PAYMENT_AMOUNT: params[:amount],
+        WMI_CURRENCY_ID: @account.walletone_currency,
+        WMI_PAYMENT_NO: params[:transaction_id],
+        WMI_DESCRIPTION: "BASE64:#{Base64.encode64(params[:description])}",
+        WMI_SUCCESS_URL: order_path,
+        WMI_FAIL_URL: order_path,
+        WMI_RECIPIENT_LOGIN: params[:email] || params[:phone],
+        WMI_CUSTOMER_FIRSTNAME: client_name,
+        WMI_CUSTOMER_LASTNAME: client_surname,
+        WMI_CUSTOMER_EMAIL: params[:email],
+    }
+    wmi_params.merge!(params)
+    initialize_api(@account)
+    payment_gateway = InsalesApi::PaymentGateway.find(@account.payment_gateway_id)
+    wmi_params[:WMI_SIGNATURE] = walletone_signature(wmi_params, payment_gateway.password)
+    wmi_params
+  end
+
+  def calculate_insales_params(params)
+    insales_params = { #порядок важен для вычисления подписи
+      shop_id: params[:WMI_MERCHANT_ID],
+      amount: params[:WMI_PAYMENT_AMOUNT],
+      transaction_id: params[:transaction_id],
+      key: params[:key],
+      paid: 1
+    }
+    initialize_api(@account)
+    insales_params[:signature] = insales_signature(insales_params, @payment_gateway.password)
+    insales_params
+  end
+
+  def payment_valid?(params)
+    responce_signature = params.delete(:WMI_SIGNATURE)
+    calculated_signature = walletone_signature(params.except(:action, :controller), @payment_gateway.password)
+    responce_signature == calculated_signature
+  end
+
+  def get_client_fio(order_id)
+    order = InsalesApi::Order.find(order_id)
+    fio = order.client.name
+    fio.split(' ')
+  end
+
+  def initialize_api(account)
+    insales_app = InsalesApi::App.new(@account.domain, @account.password)
+    insales_app.configure_api
+  end
 
   def insales_result_path(path_type)
     "http://#{@account.domain}/payments/external/#{@account.payment_gateway_id}/#{path_type.to_s}"
   end
 
   def insales_order_path(key)
-    "http://#{@account.domain}/order/#{key}"
+    "http://#{@account.domain}/orders/#{key}"
   end
 
   def insales_signature(params, password)
