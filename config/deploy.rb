2f36c0be632e5f2834c654d44fb53fd028651d6c
Yuri Artemev <i@artemeff.com>
2015-04-30 18:18:55 +0300

    add config/application.yml to linked_files in deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :git_enable_submodules, 1
 # set :pty, true
 set :keep_releases, 5
 
-set :linked_files, %w{config/database.yml config/secrets.yml config/env} 
+set :linked_files, %w{config/database.yml config/secrets.yml config/application.yml config/env} 
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
