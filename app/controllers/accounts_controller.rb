debd314a9680091b0a753a4a7e0b8dee528a2da4
Yuri Artemev <i@artemeff.com>
2015-05-05 16:33:48 +0300

    poor fix
    fckn' hack, but just works as expected

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,7 +3,7 @@ class AccountsController < ApplicationController
   skip_before_action :configure_api, except: [:autologin]
 
   def autologin
-    if current_app && current_app.authorize(params[:token])
+    if current_app && auth_token == params[:token]
       redirect_to location || root_path
     else
       redirect_to Settings.redirect_url
@@ -31,6 +31,10 @@ class AccountsController < ApplicationController
   end
 
 private
+
+  def auth_token
+    InsalesApi::Password.create(current_app.password, current_app.salt)
+  end
 
   def account_params
     params.require(:account).permit(:payment_gateway_id,
